df4ecc45c8ece354e4eb2a2962be0c255b85f7eb
LWPando <mashkiica@gmail.com>
2022-03-29 09:17:34 +0200

    Baza za Vaje

---
+++ BazaZaVaje/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazaZaVaje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BazaZaVajeEntities context = new BazaZaVajeEntities();
+            //vsi dobavitelji
+            var x1 = from a in context.DOBAVITELJ
+                     select a;
+            foreach(var y in x1)
+            {
+                Console.WriteLine(y.D_IME + " " + y.D_KONTAKT);
+            }
+            Console.ReadLine();
+        }
+    }
+}
